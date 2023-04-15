c109b849505c8419cb2212fdecb5eca37d3aee13
SARITA-GOCHHAYAT <saritagochhayat7377@gmail.com>
2023-04-15 11:35:30 +0530

    just updated

--- login.js
+++ login.js
@@ -11,16 +11,37 @@ let lsSign=JSON.parse(localStorage.getItem("signdata"))||[];
   });
   login.addEventListener("click",function(event){
     event.preventDefault();
+    if(email.value==""||password.value==""){
+        alert("Please fill the all inputs.😑");
+    }else{
+        let flag=false;
 lsSign.forEach(function(e) {
     if(e.email === email.value && e.password ===password.value){
-        alert("login successfully");
-        window.location.href= "http://127.0.0.1:5501/product-bag.html";
-    }else if(e.email !== email.value && e.password ===password.value || e.email === email.value && e.password !==password.value ){
-   alert("wrong credential");
+        flag=true;
+     return flag;
     }
+    
+});
+if(flag==true){
+    alert("login successfully");
+    window.location.href= "http://127.0.0.1:5501/product-bag.html";
+}else{
+    let a=false;
+lsSign.forEach(function(f){
+    if( e.email === email.value && e.password !==password.value ){
+    a=true;
+    return a;
+    }
+})
+if(a==true){
+    alert("wrong credential");
+    password.value="";
+}
     else{
         alert("new user");
         window.location.href= "http://127.0.0.1:5501/signin.html";
-    } 
+}
+} 
+    }
 });
-});
+ 
--- signin.js
+++ signin.js
@@ -12,10 +12,23 @@ login.addEventListener("click",function(event){
     event.preventDefault();
 window.location.href="http://127.0.0.1:5501/login.html";
  });
-   
-
-
  signin.addEventListener("click",function(event){
+    if(nameinp.value=="" ||email.value==""||password.value==""){
+        alert("Please fill the all inputs.😑");
+    }
+    else{
+        let flag=false;
+        lsSign.forEach(ele => {
+            if(ele.email==email.value){
+            flag=true;
+        return flag;
+            }
+        });
+    
+    if(flag==true){
+        window.location.href="http://127.0.0.1:5501/login.html";  
+        alert("Hey! You are a exsisting User.Please login😚"); 
+    }else{
     event.preventDefault();
     let obj={
         name:nameinp.value,
@@ -26,5 +39,7 @@ window.location.href="http://127.0.0.1:5501/login.html";
         localStorage.setItem("signdata",JSON.stringify(lsSign));
         alert("signin successfully");
         window.location.href= "http://127.0.0.1:5501/product-bag.html";
+    }
+}
  });
   
